a7c9a4ff21398cd80b5a3f54b0633031a11cfbe9
Cesar <cesar-pimenta@live.com>
2019-09-18 17:25:18 -0300

    ateração de BD

--- src/CatalotecaWeb.Data/Context/ContextFactory.cs
+++ src/CatalotecaWeb.Data/Context/ContextFactory.cs
@@ -7,7 +7,7 @@ namespace CatalotecaWeb.Data.Context
     {
         public MyContext CreateDbContext(string[] args)
         {
-            var connectionString = "Server=localhost;Port=3306;Database=dbCatalotecaWeb;Uid=root;Pwd=Chile@2019";
+            var connectionString = "Server=localhost;Port=3306;Database=dbCatalotecaWeb;Uid=root;Pwd=";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql (connectionString);
             return new MyContext(optionsBuilder.Options);
