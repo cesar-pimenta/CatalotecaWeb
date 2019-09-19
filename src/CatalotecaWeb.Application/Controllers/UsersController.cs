2c7b351d7325b313d32a86da69fab8cbd7bb67d2
Cesar <cesar-pimenta@live.com>
2019-09-19 17:00:50 -0300

    implementação de metodo GETALL

--- src/CatalotecaWeb.Application/Controllers/UsersController.cs
+++ src/CatalotecaWeb.Application/Controllers/UsersController.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using CatalotecaWeb.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
 namespace CatalotecaWeb.Application.Controllers
 {
-    public class UsersController
+    //http://localhost:5000/api/users
+    [Route ("api/[controller]")]
+    [ApiController]  
+    public class UsersController : ControllerBase
     {
-        
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            // Primeir coisa a validar é o state
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); // 400 Bad request - Solicitação Invalida
+            }
+            try
+            {
+                
+            }
+            catch (ArgumentException errors)
+            {
+                // tive que sair ...  Amanha continuo 
+            }
+        }
     }
 }
