243b6897e122ad2c76b256200277946a440c4fc3
Cesar <cesar-pimenta@live.com>
2019-09-19 15:31:20 -0300

    Metodos - camada DATA

--- src/CatalotecaWeb.Data/Repository/BaseRepository.cs
+++ src/CatalotecaWeb.Data/Repository/BaseRepository.cs
@@ -4,38 +4,109 @@ using System.Threading.Tasks;
 using CatalotecaWeb.Data.Context;
 using CatalotecaWeb.Domain.Entities;
 using CatalotecaWeb.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatalotecaWeb.Data.Repository
 {
     public class BaseRepository<T> : IRepository<T> where T : BaseEntity
-    {
+    {   
+        // Disponibiliza o metodo para fora da class
+        protected readonly MyContext _context;
+        private DbSet<T> _dataset;
         public BaseRepository(MyContext context)
         {
-            
+            _context = context;
+            _dataset = _context.Set<T>();
         }
-        public Task<bool> DeleteAsync(Guid id)
+        public async Task<T> InsertAsync (T item){
+            try
+            {
+                // Analisa se o ID está vazio, se tiver vazio ele atribui um novo Guid. 
+                if (item.Id == Guid.Empty){
+                    item.Id = Guid.NewGuid();
+                } 
+                // Recebe a data atual. 
+                item.CreateAt = DateTime.UtcNow;
+                _dataset.Add(item);
+
+                //Salva no banco
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return item;
+        }
+        public async Task<T> UpdateAsync(T item)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // Seleciona no banco o ID do item e guarda nessa variavel.
+                var result = await _dataset.SingleOrDefaultAsync( p => p.Id.Equals(item.Id));
+                // Garantir que qualquer dado que não seja do banco de dados, altere alguma coisa do banco de dados.
+                if (result == null)
+                    return null;
+                // Grava a Alteração
+                item.UpdateAt = DateTime.UtcNow;
+                // Mantem data de Criação
+                item.CreateAt = result.CreateAt;
+
+                _context.Entry(result).CurrentValues.SetValues(item);
+                await _context.SaveChangesAsync();                
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
+            return item;
+        }
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var result = await _dataset.SingleOrDefaultAsync( p => p.Id.Equals(id));
+                if (result == null)
+                    return false;                
+
+                _dataset.Remove(result);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+        public async Task<bool> ExistAsync (Guid id){
+            return await _dataset.AnyAsync ( p => p.Id.Equals (id));
+        }
+        public async Task<T> SelectAsync(Guid id)
+        {
+            try
+            {
+                return await _dataset.SingleOrDefaultAsync (p => p.Id.Equals (id));
+            }
+            catch (Exception ex) 
+            {
+                throw ex;
+            }
+        }
+        public async Task<IEnumerable<T>> SelectAsync()
+        {
+            try
+            {
+                // Retorno de lista, mas pesquisar a melhor forma de fazer com WHERE. 
+                return await _dataset.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
         }
 
-        public Task<T> InterAsync(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> SelectAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<T>> SelectAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> UpdateAsync(T item)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
--- src/CatalotecaWeb.Domain/Interfaces/IRepository.cs
+++ src/CatalotecaWeb.Domain/Interfaces/IRepository.cs
@@ -8,10 +8,11 @@ namespace CatalotecaWeb.Domain.Interfaces
     // T é uma referencia a TABLE, mas pode ser qualquer Letra ou palavra. 
     public interface IRepository<T> where T:BaseEntity
     {
-         Task<T> InterAsync (T item);
+         Task<T> InsertAsync(T item);
          Task<T> UpdateAsync (T item);
          Task<bool> DeleteAsync(Guid id);
          Task<T> SelectAsync(Guid id);
          Task<IEnumerable<T>> SelectAsync();
+         Task<bool> ExistAsync(Guid id);
     }
 }
